a3431b55bfb678c27fa164d3a3a780430a52693d
2719357 Adote Jovani AKUE-GOEH <2719357@collegelacite.ca>
2024-12-09 16:27:27 -0500

    Component demandeConges Employe

--- PresenceManagementInterface/src/router/index.js
+++ PresenceManagementInterface/src/router/index.js
@@ -2,6 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/components/home.vue'
 import AjoutRapport from '@/components/rapports/AjoutRapport.vue'
 import ModifierRapport from '@/components/rapports/ModifierRapport.vue'
+
+import AjoutEmployes from '@/components/Employes/AjoutEmployes.vue'
+import ModifierEmployes from '@/components/Employes/ModifierEmployes.vue'
+
+import AjoutDemandesConges from '@/components/demandeConges/AjoutDemandesConges.vue'
+import ModifierDemandesConges from '@/components/demandesConges/ModifierDemandesConges.vue'
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -22,7 +31,32 @@ const router = createRouter({
       path: '/modifierRapport/:id',
       name: 'ModifierRapport',
       component: ModifierRapport,
-    }
+    },
+
+    //Route ajout employé
+    {
+      path: '/ajoutEmployes',
+      name: 'AjoutEmployes',
+      component: AjoutEmployes,
+    },
+    //Route modification employé
+    {
+      path: '/modifierEmployes/:id',
+      name: 'ModifierEmployes',
+      component: ModifierEmployes,
+    },
+    //Route ajout demande de congés
+    {
+      path: '/ajoutDemandesConges',
+      name: 'AjoutDemandesConges',
+      component: AjoutDemandesConges,
+    },
+    //Route modification demande de congés
+    {
+      path: '/modifierDemandesConges/:id',
+      name: 'ModifierDemandesConges',
+      component: ModifierDemandesConges,
+    },
   ],
 })
 
